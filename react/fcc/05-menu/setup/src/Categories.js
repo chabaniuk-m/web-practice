45f1e4c7f3406690c5130e7c65dee418b300e241
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-11 22:45:08 +0200

    projects setup

--- react/fcc/05-menu/setup/src/Categories.js
+++ react/fcc/05-menu/setup/src/Categories.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({categories, filterMeals}) => {
+  return (
+    <div className='menu'>
+      <h2 className='title'>Our Menu</h2>
+      <div className='underline'></div>
+      <div className='btn-container'>
+        <button className='filter-btn' onClick={() => filterMeals()}>all</button>
+        {
+          categories().map(category => {
+            return <button className='filter-btn' onClick={() => filterMeals(category)}>{category}</button>
+          })
+        }
+      </div>
+    </div>
+  )
 };
 
 export default Categories;
