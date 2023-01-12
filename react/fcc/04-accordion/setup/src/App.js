226e1071485d20cda63de3ae5aa1e42ec2b2d88f
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-12 16:26:34 +0200

    accordion project using internal data main component

--- react/fcc/04-accordion/setup/src/App.js
+++ react/fcc/04-accordion/setup/src/App.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data)
+
+  return (
+    <main>
+      <div className='container'>
+        <h3>Question And Answers About Login</h3>
+        <section>{questions.map(question => {
+          return <SingleQuestion {...question} />
+        })}</section>
+      </div>
+    </main>
+  )
 }
 
 export default App;
