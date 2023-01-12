3672d117fd0cd44d96e86345c785d477b49e2dea
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-12 16:26:44 +0200

    accordion project using internal data question component

--- react/fcc/04-accordion/setup/src/Question.js
+++ react/fcc/04-accordion/setup/src/Question.js
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({id, title, info}) => {
+  const [expanded, setExpanded] = useState(false)
+  
+  const MoreInfo = () => {
+    return expanded ? <AiOutlineMinus /> : <AiOutlinePlus />
+  }
+  const Text = () => {
+    return expanded ? <p>{info}</p> : <p></p>
+  }
+  const toggleExpand = () => {
+    setExpanded(!expanded)
+  }
+
+  return (
+    <div className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={toggleExpand}>
+          <MoreInfo />
+        </button>
+      </header>
+      <Text />
+    </div>
+  )
 };
 
 export default Question;
