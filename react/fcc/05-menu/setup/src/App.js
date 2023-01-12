09546b2b6fa1af59be2783cce438fef34ce38d25
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-12 16:27:32 +0200

    menu project using internal data main component

--- react/fcc/05-menu/setup/src/App.js
+++ react/fcc/05-menu/setup/src/App.js
@@ -4,7 +4,35 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [meals, setMeals] = useState(items);
+
+  const categories = () => {
+    const unique = []
+    for (const item of items) {
+      if (!unique.includes(item.category)) {
+        unique.push(item.category);
+      }
+    }
+    return unique
+  }
+
+  const filterMeals = (category) => {
+    if (category) {
+      setMeals(items.filter(item => item.category == category))
+    } else {
+      console.log('All is clicked')
+      setMeals(items)
+    }
+  }
+
+  return (
+    <main>
+      <Categories categories={categories} filterMeals={filterMeals} />
+      <section className='section-center'>
+        {meals.map(meal => <Menu {...meal} />)}
+      </section>
+    </main>
+  )
 }
 
 export default App;
