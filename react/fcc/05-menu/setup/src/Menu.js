2060745e5bb35c3043f6a9102a4d440bf68c6901
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-12 16:27:47 +0200

    menu project using internal data menu component

--- react/fcc/05-menu/setup/src/Menu.js
+++ react/fcc/05-menu/setup/src/Menu.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({id, title, category, price, img, desc}) => {
+  return (
+    <div className='menu-item'>
+      <img className='photo' src={img} alt={title} />
+      <div className='item-info'>
+        <header>
+          <h4>{title}</h4>
+          <span className='price'>{price}</span>
+        </header>
+        <p className='item-text'>{desc}</p>
+      </div>
+    </div>
+  )
 };
 
 export default Menu;
