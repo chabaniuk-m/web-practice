21df328bea92367a4f8a921044a0d58275c34511
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-17 22:54:40 +0200

    single color component

--- react/fcc/09-color-generator/setup/src/SingleColor.js
+++ react/fcc/09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({weight, index, hex}) => {
+  const [alert, setAlert] = useState(false)
+  const hexCode = '#' + hex
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+  
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [alert])
+  
+  return (
+    <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor: `${hexCode}`}} onClick={() => {
+      navigator.clipboard.writeText(hexCode)
+      setAlert(true)
+    }}>
+      <p className='percent-value'>
+        {weight}%
+      </p>
+      <p className='color-value'>{hexCode}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
+    </article>
+  )
 }
 
 export default SingleColor
