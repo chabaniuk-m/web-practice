8cf328e83da7c6b58f1d39bb30cc3a59d94914f9
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-19 15:34:54 +0200

    navbar component

--- react/fcc/11-navbar/setup/src/data.js
+++ react/fcc/11-navbar/setup/src/data.js
@@ -2,11 +2,6 @@ import React from 'react'
 import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
 export const links = [
   {
-    id: 1,
-    url: '/',
-    text: 'random',
-  },
-  {
     id: 2,
     url: '/about',
     text: 'about',
