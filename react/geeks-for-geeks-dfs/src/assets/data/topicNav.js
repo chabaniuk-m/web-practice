73f4e919c93a3a331eef7ded29f4b670a6c35a2e
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-24 01:49:52 +0200

    links for topics data are added

---
+++ react/geeks-for-geeks-dfs/src/assets/data/topicNav.js
@@ -0,0 +1,465 @@
+export default [
+  {
+    title: "Write an Article",
+    topics: [
+      {
+        title: "Write Articles",
+        url: "https://write.geeksforgeeks.org/posts-new/?ref=GLBWA"
+      },
+      {
+        title: "Pick Topics to write",
+        url: "https://write.geeksforgeeks.org/pick-article/?ref=GLBPT"
+      },
+      {
+        title: "Guidelines to Write",
+        url: "https://write.geeksforgeeks.org/how-to-write/?ref=GLBHW"
+      },
+      {
+        title: "Get Technical Writing Internship",
+        url: "https://www.geeksforgeeks.org/get-technical-content-writing-internship-geeksforgeeks-org/"
+      }
+    ]
+  },
+  {
+    title: "Write an Interview Experience",
+    url: "https://write.geeksforgeeks.org/posts-new?interview-experience/?ref=GLBIE"
+  },
+  {
+    title: "Graph Data Structure",
+    url: "https://www.geeksforgeeks.org/graph-data-structure-and-algorithms/?ref=lbp"
+  },
+  {
+    title: "Introduction",
+    topics: [
+      {
+        title: "Introduction to Graphs - Data Structure and Algorithm Tutorials",
+        url: "https://www.geeksforgeeks.org/introduction-to-graphs-data-structure-and-algorithm-tutorials/?ref=lbp"
+      },
+      {
+        title: "Graph and its representations",
+        url: "https://www.geeksforgeeks.org/graph-and-its-representations/?ref=lbp"
+      },
+      {
+        title: "Graph representations using set and hash",
+        url: "https://www.geeksforgeeks.org/graph-representations-using-set-hash/?ref=lbp"
+      },
+      {
+        title: "Transpose graph",
+        url: "https://www.geeksforgeeks.org/transpose-graph/?ref=lbp"
+      },
+      {
+        title: "Check whether a given graph is Bipartile or not",
+        url: "https://www.geeksforgeeks.org/bipartite-graph/?ref=lbp"
+      },
+      {
+        title: "Applications, Advantages and Disadvantages of Graph",
+        url: "https://www.geeksforgeeks.org/applications-advantages-and-disadvantages-of-graph/?ref=lbp"
+      },
+      {
+        title: "Applications, Advantages and Disadvantages of Unweighted Graph",
+        url: "https://www.geeksforgeeks.org/applications-advantages-and-disadvantages-of-unweighted-graph/?ref=lbp"
+      },
+      {
+        title: "Applications, Advantages and Disadvantages of Weighted Graph",
+        url: "https://www.geeksforgeeks.org/applications-advantages-and-disadvantages-of-weighted-graph/?ref=lbp"
+      },
+      {
+        title: "Applications, Advantages and Disadvantages of Directed Graph",
+        url: "https://www.geeksforgeeks.org/applications-advantages-and-disadvantages-of-directed-graph/?ref=lbp"
+      },
+    ]
+  },
+  {
+    title: "BFS and DFS on Graph",
+    topics: [
+      {
+        title: "Breadth First Search or BFS for a Graph",
+        url: "https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/?ref=lbp"
+      },
+      {
+        title: "Depth First Search or DFS for a Graph",
+        url: "https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/?ref=lbp"
+      },
+      {
+        title: "Applications of Depth First Search",
+        url: "https://www.geeksforgeeks.org/applications-of-depth-first-search/?ref=lbp"
+      },
+      {
+        title: "Applications of Breadth First Traversal",
+        url: "https://www.geeksforgeeks.org/applications-of-breadth-first-traversal/?ref=lbp"
+      },
+      {
+        title: "Count the number of nodes at given level in a tree using BFS",
+        url: "https://www.geeksforgeeks.org/count-number-nodes-given-level-using-bfs/?ref=lbp"
+      },
+      {
+        title: "Count all possible Paths between two Vertices",
+        url: "https://www.geeksforgeeks.org/count-possible-paths-two-vertices/?ref=lbp"
+      },
+      {
+        title: "BFS using STL for competitive coding",
+        url: "https://www.geeksforgeeks.org/bfs-using-stl-competitive-coding/?ref=lbp"
+      },
+      {
+        title: "Clone an Undirected Graph",
+        url: "https://www.geeksforgeeks.org/clone-an-undirected-graph/?ref=lbp"
+      },
+      {
+        title: "Bidirectional Search",
+        url: "https://www.geeksforgeeks.org/bidirectional-search/?ref=lbp"
+      },
+      {
+        title: "Best First Search (Informed Search)",
+        url: "https://www.geeksforgeeks.org/best-first-search-informed-search/?ref=lbp"
+      },
+      {
+        title: "BFS for Disconnected Graph",
+        url: "https://www.geeksforgeeks.org/bfs-disconnected-graph/?ref=lbp"
+      },
+    ]
+  },
+  {
+    title: "Cycle in a graph",
+    topics: [
+      {
+        title: "Detect Cycle in a Directed Graph",
+        url: "https://www.geeksforgeeks.org/detect-cycle-in-a-graph/?ref=lbp"
+      },
+      {
+        title: "Detect cycle in an undirected graph",
+        url: "https://www.geeksforgeeks.org/detect-cycle-undirected-graph/?ref=lbp"
+      },
+      {
+        title: "Detect a negative cycle in a Graph | (Bellman Ford)",
+        url: "https://www.geeksforgeeks.org/detect-negative-cycle-graph-bellman-ford/?ref=lbp"
+      },
+      {
+        title: "Cycles of length n in an undirected and connected graph",
+        url: "https://www.geeksforgeeks.org/cycles-of-length-n-in-an-undirected-and-connected-graph/?ref=lbp"
+      },
+      {
+        title: "Detecting negative cycle using Floyd Warshall",
+        url: "https://www.geeksforgeeks.org/detecting-negative-cycle-using-floyd-warshall/?ref=lbp"
+      },
+      {
+        title: "Detect Cycle in a directed graph using colors",
+        url: "https://www.geeksforgeeks.org/detect-cycle-direct-graph-using-colors/?ref=lbp"
+      },
+      {
+        title: "Clone a Directed Acyclic Graph",
+        url: "https://www.geeksforgeeks.org/clone-directed-acyclic-graph/?ref=lbp"
+      },
+      {
+        title: "Introduction to Disjoint Set Data Structure or Union-Find Algorithm",
+        url: "https://www.geeksforgeeks.org/introduction-to-disjoint-set-data-structure-or-union-find-algorithm/?ref=lbp"
+      },
+      {
+        title: "Union By Rank and Path Compression in Union-Find Algorithm",
+        url: "https://www.geeksforgeeks.org/union-by-rank-and-path-compression-in-union-find-algorithm/?ref=lbp"
+      },
+    ]
+  },
+  {
+    title: "Shortest Path in Graph",
+    topics: [
+      {
+        title: "Dijkstra's Shortest Path Algorithm | Greedy Algo-7",
+        url: "https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/?ref=lbp"
+      },
+      {
+        title: "Bellman-Ford Algorithm | DP-23",
+        url: "https://www.geeksforgeeks.org/bellman-ford-algorithm-dp-23/?ref=lbp"
+      },
+      {
+        title: "Floyd Warshall Algorithm | DP-16",
+        url: "https://www.geeksforgeeks.org/floyd-warshall-algorithm-dp-16/?ref=lbp"
+      },
+      {
+        title: "Johnson's algorithm for All-pairs shortest paths",
+        url: "https://www.geeksforgeeks.org/johnsons-algorithm/?ref=lbp"
+      },
+      {
+        title: "Shortest Path in Directed Acyclic Graph",
+        url: "https://www.geeksforgeeks.org/shortest-path-for-directed-acyclic-graphs/?ref=lbp"
+      },
+      {
+        title: "Shortest path in an unweighted graph",
+        url: "https://www.geeksforgeeks.org/shortest-path-unweighted-graph/?ref=lbp"
+      },
+      {
+        title: "Comparison of Dijkstra's and Floyd-Warshall algorithms",
+        url: "https://www.geeksforgeeks.org/comparison-dijkstras-floyd-warshall-algorithms/?ref=lbp"
+      },
+      {
+        title: "Find minimum weight cycle in undirected graph",
+        url: "https://www.geeksforgeeks.org/find-minimum-weight-cycle-undirected-graph/?ref=lbp"
+      },
+      {
+        title: "Find Shortest distance from a guard in a Bank",
+        url: "https://www.geeksforgeeks.org/find-shortest-distance-guard-bank/?ref=lbp"
+      },
+    ]
+  },
+  {
+    title: "Topological Sorting in Graph",
+    topics: [
+      {
+        title: "Topological Sorting",
+        url: "https://www.geeksforgeeks.org/topological-sorting/?ref=lbp"
+      },
+      {
+        title: "All Topological Sorts of a Directed Acyclic Graph",
+        url: "https://www.geeksforgeeks.org/all-topological-sorts-of-a-directed-acyclic-graph/?ref=lbp"
+      },
+      {
+        title: "Kahn's algorithm for Topological Sorting",
+        url: "https://www.geeksforgeeks.org/topological-sorting-indegree-based-solution/?ref=lbp"
+      },
+      {
+        title: "Maximum edges that can be added to DAG so that it remains DAG",
+        url: "https://www.geeksforgeeks.org/maximum-edges-can-added-dag-remains-dag/?ref=lbp"
+      },
+      {
+        title: "Longest Path in a Directed Acyclic Graph",
+        url: "https://www.geeksforgeeks.org/find-longest-path-directed-acyclic-graph/?ref=lbp"
+      },
+      {
+        title: "Given a sorted dictionary of an alien language, find order of characters",
+        url: "https://www.geeksforgeeks.org/given-sorted-dictionary-find-precedence-characters/?ref=lbp"
+      },
+      {
+        title: "Find the ordering of tasks from given dependencies",
+        url: "https://www.geeksforgeeks.org/find-the-ordering-of-tasks-from-given-dependencies/?ref=lbp"
+      },
+      {
+        title: "Topological Sort of a graph using departure time of vertex",
+        url: "https://www.geeksforgeeks.org/topological-sorting-using-departure-time-of-vertex/?ref=lbp"
+      },
+    ]
+  },
+  {
+    title: "Minimum Spanning Tree",
+    topics: [
+      {
+        title: "Prim's Minimum Spanning Tree (MST) | Greedy Algo-5",
+        url: "https://www.geeksforgeeks.org/prims-minimum-spanning-tree-mst-greedy-algo-5/?ref=lbp"
+      },
+      {
+        title: "Applications of Minimum Spanning Tree Problem",
+        url: "https://www.geeksforgeeks.org/applications-of-minimum-spanning-tree/?ref=lbp"
+      },
+      {
+        title: "Kruskal's Minimum Spanning Tree Algorithm | Greedy Algo-2",
+        url: "https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-algorithm-greedy-algo-2/?ref=lbp"
+      },
+      {
+        title: "Boruvka's algorithm | Greedy Algo-9",
+        url: "https://www.geeksforgeeks.org/boruvkas-algorithm-greedy-algo-9/?ref=lbp"
+      },
+      {
+        title: "Steiner Tree",
+        url: "https://www.geeksforgeeks.org/steiner-tree/?ref=lbp"
+      },
+      {
+        title: "Minimum cost to connect all cities",
+        url: "https://www.geeksforgeeks.org/minimum-cost-connect-cities/?ref=lbp"
+      },
+      {
+        title: "Total number of Spanning Trees in a Graph",
+        url: "https://www.geeksforgeeks.org/total-number-spanning-trees-graph/?ref=lbp"
+      },
+      {
+        title: "Minimum Product Spanning Tree",
+        url: "https://www.geeksforgeeks.org/minimum-product-spanning-tree/?ref=lbp"
+      },
+      {
+        title: "",
+        url: ""
+      },
+    ]
+  },
+  {
+    title: "Connectivity of Graph",
+    topics: [
+      {
+        title: "Check if a graph is strongly connected | Set 1 (Kosaraju using DFS)",
+        url: "https://www.geeksforgeeks.org/connectivity-in-a-directed-graph/?ref=lbp"
+      },
+      {
+        title: "Strongly connected components",
+        url: "https://www.geeksforgeeks.org/strongly-connected-components/?ref=lbp"
+      },
+      {
+        title: "Tarjan's Algorithm to find Strongly Connected Components",
+        url: "https://www.geeksforgeeks.org/tarjan-algorithm-find-strongly-connected-components/?ref=lbp"
+      },
+      {
+        title: "Connected Components in an Undirected Graph",
+        url: "https://www.geeksforgeeks.org/connected-components-in-an-undirected-graph/?ref=lbp"
+      },
+      {
+        title: "Biconnected graph",
+        url: "https://www.geeksforgeeks.org/biconnectivity-in-a-graph/?ref=lbp"
+      },
+      {
+        title: "Biconnected Components",
+        url: "https://www.geeksforgeeks.org/biconnected-components/?ref=lbp"
+      },
+      {
+        title: "Eulerian path and circuit for undirected graph",
+        url: "https://www.geeksforgeeks.org/eulerian-path-and-circuit/?ref=lbp"
+      },
+      {
+        title: "Fleury's Algorithm for printing Eulerian Path or Ciruit",
+        url: "https://www.geeksforgeeks.org/fleurys-algorithm-for-printing-eulerian-path/?ref=lbp"
+      },
+      {
+        title: "Bridges in graph",
+        url: "https://www.geeksforgeeks.org/bridge-in-a-graph/?ref=lbp"
+      },
+      {
+        title: "Articulation Points (or Cut Vertices) in a Graph",
+        url: "https://www.geeksforgeeks.org/articulation-points-or-cut-vertices-in-a-graph/?ref=lbp"
+      },
+      {
+        title: "Transitive closure of a graph",
+        url: "https://www.geeksforgeeks.org/transitive-closure-of-a-graph/?ref=lbp"
+      },
+      {
+        title: "Find the number of islands using DFS",
+        url: "https://www.geeksforgeeks.org/find-the-number-of-islands-using-dfs/?ref=lbp"
+      },
+      {
+        title: "Euler Circuit in a Directed Graph",
+        url: "https://www.geeksforgeeks.org/euler-circuit-directed-graph/?ref=lbp"
+      },
+      {
+        title: "Eulerian Path in undirected graph",
+        url: "https://www.geeksforgeeks.org/eulerian-path-undirected-graph/?ref=lbp"
+      },
+      {
+        title: "Dynamic Connectivity | Set 1 (Incremental)",
+        url: "https://www.geeksforgeeks.org/dynamic-connectivity-set-1-incremental/?ref=lbp"
+      },
+    ]
+  },
+  {
+    title: "Maximum flow in a Graph",
+    topics: [
+      {
+        title: "Maximum Bipartite Matching",
+        url: "https://www.geeksforgeeks.org/maximum-bipartite-matching/?ref=lbp"
+      },
+      {
+        title: "Channel Assignment Problem",
+        url: "https://www.geeksforgeeks.org/channel-assignment-problem/?ref=lbp"
+      },
+      {
+        title: "Ford-Fulkerson Algorithm for Maximum Flow Problem",
+        url: "https://www.geeksforgeeks.org/ford-fulkerson-algorithm-for-maximum-flow-problem/?ref=lbp"
+      },
+      {
+        title: "Push Relabel Algorithm",
+        url: "https://www.geeksforgeeks.org/introduction-to-push-relabel-algorithm/?ref=lbp"
+      },
+      {
+        title: "Introduction to Push Relabel Algorithm | Set 2 (Implementation)",
+        url: "https://www.geeksforgeeks.org/push-relabel-algorithm-set-2-implementation/?ref=lbp"
+      },
+      {
+        title: "Introduction and implementation of Karger's algorithm for Minimum Cut",
+        url: "https://www.geeksforgeeks.org/introduction-and-implementation-of-kargers-algorithm-for-minimum-cut/?ref=lbp"
+      },
+      {
+        title: "Dinic's algorithm for Masimum Flow",
+        url: "https://www.geeksforgeeks.org/dinics-algorithm-maximum-flow/?ref=lbp"
+      },
+      {
+        title: "Max Flow Problem Introduction",
+        url: "https://www.geeksforgeeks.org/max-flow-problem-introduction/?ref=lbp"
+      },
+    ]
+  },
+  {
+    title: "Some Advanced problems of Graph",
+    topics: [
+      {
+        title: "Graph Coloring | Set 1 (Introduction and Application)",
+        url: "https://www.geeksforgeeks.org/graph-coloring-applications/?ref=lbp"
+      },
+      {
+        title: "Traveling Salesman Problem (TSP) Implementation",
+        url: "https://www.geeksforgeeks.org/traveling-salesman-problem-tsp-implementation/?ref=lbp"
+      },
+      {
+        title: "Traveling Salesman Problem using Dynamic Programming",
+        url: "https://www.geeksforgeeks.org/travelling-salesman-problem-using-dynamic-programming/?ref=lbp"
+      },
+      {
+        title: "Approximate solution for Traveling Salesman Problem using MST",
+        url: "https://www.geeksforgeeks.org/approximate-solution-for-travelling-salesman-problem-using-mst/?ref=lbp"
+      },
+      {
+        title: "Introduction and Approximate Solution for Vertex Cover Problem",
+        url: "https://www.geeksforgeeks.org/introduction-and-approximate-solution-for-vertex-cover-problem/?ref=lbp"
+      },
+      {
+        title: "Greedy Approximate Algorithm for K Centers Problem",
+        url: "https://www.geeksforgeeks.org/greedy-approximate-algorithm-for-k-centers-problem/?ref=lbp"
+      },
+      {
+        title: "Erdos Renyl Model (for generating Random Graphs)",
+        url: "https://www.geeksforgeeks.org/erdos-renyl-model-generating-random-graphs/?ref=lbp"
+      },
+      {
+        title: "Chinese Postman or Route Inspection | Set 1 (Introduction)",
+        url: "https://www.geeksforgeeks.org/chinese-postman-route-inspection-set-1-introduction/?ref=lbp"
+      },
+      {
+        title: "Hierholzer's Algorithm for directed graph",
+        url: "https://www.geeksforgeeks.org/hierholzers-algorithm-directed-graph/?ref=lbp"
+      },
+    ]
+  },
+  {
+    title: "Some interesting problems on Graph",
+    topics: [
+      {
+        title: "Number of Triangles in an Undirected Graph",
+        url: "https://www.geeksforgeeks.org/number-of-triangles-in-a-undirected-graph/?ref=lbp"
+      },
+      {
+        title: "Snake and Ladder Problem",
+        url: "https://www.geeksforgeeks.org/snake-ladder-problem-2/?ref=lbp"
+      },
+      {
+        title: "Minimum time required to rot all oranges",
+        url: "https://www.geeksforgeeks.org/minimum-time-required-so-that-all-oranges-become-rotten/?ref=lbp"
+      },
+      {
+        title: "Construct a graph from given degrees of all vertices",
+        url: "https://www.geeksforgeeks.org/construct-graph-given-degrees-vertices/?ref=lbp"
+      },
+      {
+        title: "Degree Centrality (Centrality Measure)",
+        url: "https://www.geeksforgeeks.org/degree-centrality-centrality-measure/?ref=lbp"
+      },
+      {
+        title: "Number of sink nodes in a graph",
+        url: "https://www.geeksforgeeks.org/number-sink-nodes-graph/?ref=lbp"
+      },
+      {
+        title: "Count number of edges in an undirected graph",
+        url: "https://www.geeksforgeeks.org/count-number-edges-undirected-graph/?ref=lbp"
+      },
+      {
+        title: "Two Clique Problem (Check if Graph can be divided in two Cliques)",
+        url: "https://www.geeksforgeeks.org/two-clique-problem-check-graph-can-divided-two-cliques/?ref=lbp"
+      },
+      {
+        title: "Sum of dependencies in a graph",
+        url: "https://www.geeksforgeeks.org/sum-dependencies-graph/?ref=lbp"
+      },
+    ]
+  }
+]
