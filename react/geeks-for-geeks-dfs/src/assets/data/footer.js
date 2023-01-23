0e78ee4af9d820fdc872301dc17587c5ab0e42ec
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-24 00:45:09 +0200

    footer links data

--- react/geeks-for-geeks-dfs/src/assets/data/footer.js
+++ react/geeks-for-geeks-dfs/src/assets/data/footer.js
@@ -1,47 +1,204 @@
-export const improvedBy = `speak2rk09
-techno2mahi
-princiraj1992
-eshankvaish
-andrew1234
-draco_malf0y
-nikhil104
-akashgoac
-itisvishnudev
-koushalsagar66
-rdtank
-avanitrachhadiya2155
-dheerajkumar33
-tanvimoharir
-byromjomaa
-amartyaghoshgfg
-surinderdawra388
-mitalibhola94
-aayushi2402
-janardansthox
-pk03`.split("\n")
-
-export const articleTags = [
+export default [
   {
-    title: "DFS",
-    url: "https://www.geeksforgeeks.org/tag/dfs/"
+    title: "Company",
+    topics: [
+      {
+        title: "About Us",
+        url: "https://www.geeksforgeeks.org/about/?ref=footer"
+      },
+      {
+        title: "Careers",
+        url: "https://geeksforgeeks.zohorecruit.in/careers"
+      },
+      {
+        title: "In Media",
+        url: "https://www.geeksforgeeks.org/press-release/?ref=footer"
+      },
+      {
+        title: "Contact Us",
+        url: "https://www.geeksforgeeks.org/about/contact-us/?ref=footer"
+      },
+      {
+        title: "Privacy Policy",
+        url: "https://www.geeksforgeeks.org/privacy-policy/?ref=footer"
+      },
+      {
+        title: "Copyright Policy",
+        url: "https://www.geeksforgeeks.org/copyright-information/?ref=footer"
+      },
+      {
+        title: "Advertise with us",
+        url: "https://www.geeksforgeeks.org/advertise-with-us/?ref=footer"
+      },
+    ]
   },
   {
-    title: "graph-basics",
-    url: "https://www.geeksforgeeks.org/tag/graph-basics/"
+    title: "Learn",
+    topics: [
+      {
+        title: "DSA",
+        url: "https://www.geeksforgeeks.org/learn-data-structures-and-algorithms-dsa-tutorial/?ref=footer"
+      },
+      {
+        title: "Algorithms",
+        url: "https://www.geeksforgeeks.org/fundamentals-of-algorithms/?ref=footer"
+      },
+      {
+        title: "Data Structures",
+        url: "https://www.geeksforgeeks.org/data-structures/?ref=footer"
+      },
+      {
+        title: "SDE Cheat Sheet",
+        url: "https://www.geeksforgeeks.org/gfg-sde-sheet-a-complete-guide-for-sde-preparation/?ref=footer"
+      },
+      {
+        title: "Machine learning",
+        url: "https://www.geeksforgeeks.org/machine-learning/?ref=footer"
+      },
+      {
+        title: "CS Subjects",
+        url: "https://www.geeksforgeeks.org/articles-on-computer-science-subjects-gq/?ref=footer"
+      },
+      {
+        title: "Video Tutorials",
+        url: "https://www.geeksforgeeks.org/videos/?ref=footer"
+      },
+      {
+        title: "Courses",
+        url: "https://practice.geeksforgeeks.org/courses/?ref=footer"
+      },
+    ]
   },
   {
-    title: "Graph",
-    url: "https://www.geeksforgeeks.org/category/dsa/data-structures/graph/"
-  }
-]
-
-export const practiceTags = [
-  {
-    title: "DFS",
-    url: "https://www.geeksforgeeks.org/tag/dfs/"
+    title: "News",
+    topics: [
+      {
+        title: "Top News",
+        url: "https://news.geeksforgeeks.org/top?ref=footer"
+      },
+      {
+        title: "Technology",
+        url: "https://news.geeksforgeeks.org/technology?ref=footer"
+      },
+      {
+        title: "Work & Career",
+        url: "https://news.geeksforgeeks.org/work-career?ref=footer"
+      },
+      {
+        title: "Business",
+        url: "https://news.geeksforgeeks.org/business?ref=footer"
+      },
+      {
+        title: "Finance",
+        url: "https://news.geeksforgeeks.org/finance?ref=footer"
+      },
+      {
+        title: "Lifestyle",
+        url: "https://news.geeksforgeeks.org/lifestyle?ref=footer"
+      },
+      {
+        title: "Knowledge",
+        url: "https://news.geeksforgeeks.org/knowledge?ref=footer"
+      },
+    ]
   },
   {
-    title: "Graph",
-    url: "https://www.geeksforgeeks.org/category/dsa/data-structures/graph/"
+    title: "Languages",
+    topics: [
+      {
+        title: "C",
+        url: "https://www.geeksforgeeks.org/c-programming-language/?ref=ghm",
+      },
+      {
+        title: "CPP",
+        url: "https://www.geeksforgeeks.org/c-plus-plus/?ref=ghm",
+      },
+      {
+        title: "Java",
+        url: "https://www.geeksforgeeks.org/java/?ref=ghm"
+      },
+      {
+        title: "Python",
+        url: "https://www.geeksforgeeks.org/python-programming-language/?ref=ghm",
+      },
+      {
+        title: "C#",
+        url: "https://www.geeksforgeeks.org/csharp-programming-language/?ref=ghm",
+      },
+      {
+        title: "SQL",
+        url: "https://www.geeksforgeeks.org/sql-tutorial/?ref=ghm",
+      },
+      {
+        title: "Golang",
+        url: "https://www.geeksforgeeks.org/golang/?ref=ghm",
+      },
+      {
+        title: "Kotlin",
+        url: "https://www.geeksforgeeks.org/kotlin-programming-language/?ref=ghm",
+      },
+    ]
+  },
+  {
+    title: "Web Development",
+    topics: [
+      {
+        title: "Web Tutorials",
+        url: "https://www.geeksforgeeks.org/web-development/?ref=footer"
+      },
+      {
+        title: "Django Tutorials",
+        url: "https://www.geeksforgeeks.org/django-tutorial/?ref=footer"
+      },
+      {
+        title: "HTML",
+        url: "https://www.geeksforgeeks.org/html-tutorials/?ref=footer"
+      },
+      {
+        title: "JavaScript",
+        url: "https://www.geeksforgeeks.org/javascript-tutorial/?ref=footer"
+      },
+      {
+        title: "Bootstrap",
+        url: "https://www.geeksforgeeks.org/bootstrap-tutorials/?ref=footer"
+      },
+      {
+        title: "ReactJS",
+        url: "https://www.geeksforgeeks.org/reactjs-tutorials/?ref=footer"
+      },
+      {
+        title: "NodeJS",
+        url: "https://www.geeksforgeeks.org/nodejs/?ref=footer"
+      },
+    ]
+  },
+  {
+    title: "Contribute",
+    topics: [
+      {
+        title: "Write an Article",
+        url: "https://www.geeksforgeeks.org/contribute/?ref=footer"
+      },
+      {
+        title: "Improve an Article",
+        url: "https://www.geeksforgeeks.org/how-to-improve-an-article/?ref=footer"
+      },
+      {
+        title: "Pick Topics to Write",
+        url: "https://write.geeksforgeeks.org/pick-article?ref=footer"
+      },
+      {
+        title: "Write Interview Experience",
+        url: "https://www.geeksforgeeks.org/write-interview-experience/?ref=footer"
+      },
+      {
+        title: "Internship",
+        url: "https://www.geeksforgeeks.org/careers/?job_type=1&ref=footer"
+      },
+      {
+        title: "Video Internship",
+        url: "https://script.geeksforgeeks.org/on-boarding/youtube?ref=footer"
+      },
+    ]
   }
 ]
---
+++ react/geeks-for-geeks-dfs/src/assets/data/tags.js
@@ -0,0 +1,47 @@
+export const improvedBy = `speak2rk09
+techno2mahi
+princiraj1992
+eshankvaish
+andrew1234
+draco_malf0y
+nikhil104
+akashgoac
+itisvishnudev
+koushalsagar66
+rdtank
+avanitrachhadiya2155
+dheerajkumar33
+tanvimoharir
+byromjomaa
+amartyaghoshgfg
+surinderdawra388
+mitalibhola94
+aayushi2402
+janardansthox
+pk03`.split("\n")
+
+export const articleTags = [
+  {
+    title: "DFS",
+    url: "https://www.geeksforgeeks.org/tag/dfs/"
+  },
+  {
+    title: "graph-basics",
+    url: "https://www.geeksforgeeks.org/tag/graph-basics/"
+  },
+  {
+    title: "Graph",
+    url: "https://www.geeksforgeeks.org/category/dsa/data-structures/graph/"
+  }
+]
+
+export const practiceTags = [
+  {
+    title: "DFS",
+    url: "https://www.geeksforgeeks.org/tag/dfs/"
+  },
+  {
+    title: "Graph",
+    url: "https://www.geeksforgeeks.org/category/dsa/data-structures/graph/"
+  }
+]
