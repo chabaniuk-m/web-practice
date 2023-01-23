bd5cdf44ff2d9c66da01bb22a6a1ee29bb69fe84
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-24 00:23:23 +0200

    related articles data added

---
+++ react/geeks-for-geeks-dfs/src/assets/data/relatedArticles.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    title: "Top 10 Interview Questions on Depth First Search (DFS)",
+    url: "https://www.geeksforgeeks.org/top-10-interview-question-depth-first-search-dfs/?ref=rp"
+  },
+  {
+    title: "Depth First Traversal on a 2D array",
+    url: "https://www.geeksforgeeks.org/depth-first-traversal-dfs-on-a-2d-array/?ref=rp"
+  },
+  {
+    title: "Iterative Deepening Search (IDS) or Iterative Deepening Depth First Search (IDDFS)",
+    url: "https://www.geeksforgeeks.org/iterative-deepening-searchids-iterative-deepening-depth-first-searchiddfs/?ref=rp"
+  },
+  {
+    title: "Applications of Depth First Search",
+    url: "https://www.geeksforgeeks.org/applications-of-depth-first-search/?ref=rp"
+  },
+  {
+    title: "Iterative Depth First Traversal of Graph",
+    url: "https://www.geeksforgeeks.org/iterative-depth-first-traversal/?ref=rp"
+  },
+  {
+    title: "Check if a graph is strongly connected | Set 1 (Kosaraju using DFS)",
+    url: "https://www.geeksforgeeks.org/connectivity-in-a-directed-graph/?ref=rp"
+  },
+  {
+    title: "Transitive Closure of a Graph using DFS",
+    url: "https://www.geeksforgeeks.org/transitive-closure-of-a-graph-using-dfs/?ref=rp"
+  },
+  {
+    title: "Graph implementation using STL for competitive programming | Set 1 (DFS of Unweighted and Undirected)",
+    url: "https://www.geeksforgeeks.org/graph-implementation-using-stl-for-competitive-programming-set-1-dfs-of-unweighted-and-undirected/?ref=rp"
+  },
+  {
+    title: "DFS for a n-ary tree (acyclic graph) represented as adjacency list",
+    url: "https://www.geeksforgeeks.org/dfs-n-ary-tree-acyclic-graph-represented-adjacency-list/?ref=rp"
+  },
+  {
+    title: "Check if the given permutation is a valid DFS of Graph",
+    url: "https://www.geeksforgeeks.org/check-given-permutation-valid-dfs-graph/?ref=rp"
+  },
+]
