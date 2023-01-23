d1358a22ee8e34a4278f59bdd8b8c3df9b454b04
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-24 00:13:02 +0200

    slider link's data added

---
+++ react/geeks-for-geeks-dfs/src/assets/data/slider.js
@@ -0,0 +1,98 @@
+export default [
+  {
+    title: "DSA",
+    url: "https://www.geeksforgeeks.org/learn-data-structures-and-algorithms-dsa-tutorial/?ref=shm"
+  },
+  {
+    title: "Array",
+    url: "https://www.geeksforgeeks.org/array-data-structure/?ref=shm"
+  },
+  {
+    title: "Matrix",
+    url: "https://www.geeksforgeeks.org/matrix/?ref=shm"
+  },
+  {
+    title: "Strings",
+    url: "https://www.geeksforgeeks.org/string-data-structure/?ref=shm"
+  },
+  {
+    title: "Hashing",
+    url: "https://www.geeksforgeeks.org/hashing-data-structure/?ref=shm"
+  },
+  {
+    title: "Linked List",
+    url: "https://www.geeksforgeeks.org/data-structures/linked-list/?ref=shm"
+  },
+  {
+    title: "Stack",
+    url: "https://www.geeksforgeeks.org/stack-data-structure/?ref=shm"
+  },
+  {
+    title: "Queue",
+    url: "https://www.geeksforgeeks.org/queue-data-structure/?ref=shm"
+  },
+  {
+    title: "Binary Tree",
+    url: "https://www.geeksforgeeks.org/binary-tree-data-structure/?ref=shm"
+  },
+  {
+    title: "Binary Search Tree",
+    url: "https://www.geeksforgeeks.org/binary-search-tree-data-structure/?ref=shm"
+  },
+  {
+    title: "Heap",
+    url: "https://www.geeksforgeeks.org/heap-data-structure/?ref=shm"
+  },
+  {
+    title: "Graph",
+    url: "https://www.geeksforgeeks.org/graph-data-structure-and-algorithms/?ref=shm"
+  },
+  {
+    title: "Searching",
+    url: "https://www.geeksforgeeks.org/searching-algorithms/?ref=shm"
+  },
+  {
+    title: "Sorting",
+    url: "https://www.geeksforgeeks.org/sorting-algorithms/?ref=shm"
+  },
+  {
+    title: "Divide & Conquer",
+    url: "https://www.geeksforgeeks.org/divide-and-conquer/?ref=shm"
+  },
+  {
+    title: "Mathematical",
+    url: "https://www.geeksforgeeks.org/mathematical-algorithms/?ref=shm"
+  },
+  {
+    title: "Geometric",
+    url: "https://www.geeksforgeeks.org/geometric-algorithms/?ref=shm"
+  },
+  {
+    title: "Bitwise",
+    url: "https://www.geeksforgeeks.org/bitwise-algorithms/?ref=shm"
+  },
+  {
+    title: "Greedy",
+    url: "https://www.geeksforgeeks.org/greedy-algorithms/?ref=shm"
+  },
+  {
+    title: "Backtracking",
+    url: "https://www.geeksforgeeks.org/backtracking-algorithms/?ref=shm"
+  },
+  {
+    title: "Branch and Bound",
+    url: "https://www.geeksforgeeks.org/branch-and-bound-algorithm/?ref=shm"
+  },
+  {
+    title: "Dynamic Programming",
+    url: "https://www.geeksforgeeks.org/dynamic-programming/?ref=shm"
+  },
+  {
+    title: "Pattern Search",
+    url: "https://www.geeksforgeeks.org/algorithms-gq/pattern-searching/?ref=shm"
+  },
+  {
+    title: "Randomized",
+    url: "https://www.geeksforgeeks.org/randomized-algorithms/?ref=sh"
+  },
+]
